ce19e42668fd29ce1e48777e7b16558bddbba2ba
Kochy_Okami <97362574+KochyOkami@users.noreply.github.com>
2022-10-28 22:20:10 +0200

    set the command /blacklist
    avoid sync to portfolio

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -11,7 +11,7 @@ module.exports = {
         .addUserOption(option =>
             option.setName('user')
                 .setDescription('the user')
-                .setRequired(true))
+                .setRequired(false))
         .addStringOption(option =>
             option.setName('add')
                 .setDescription("Add for add the user or Remove for remove the user. Add by default")
@@ -24,7 +24,7 @@ module.exports = {
     async execute(interaction) {
         await interaction.deferReply();
         try {
-            if (await interaction.options.getString('add', false)) {
+            if (await interaction.options.getUser('user', false)) {
                 try {
                     var add = await interaction.options.getString('add', false);
 
@@ -34,59 +34,96 @@ module.exports = {
                         var settings = JSON.parse(fs.readFileSync('./settings.json', 'utf8'));
 
                         if (settings.blacklist.indexOf(user.id) != -1) {
-                            settings.blacklist.splice(settings.blacklist.indexOf(user.id), settings.blacklist.indexOf(user.id))
+                            for (var i = 0; i < settings.blacklist.length; i++) {
+
+                                if (settings.blacklist[i] === user.id) {
+
+                                    settings.blacklist.splice(i, 1);
+                                }
+
+                            }
+
+                            log.write(user + ' is not in the blacklist');
+                            const text = new EmbedBuilder()
+                                .setColor('#245078')
+                                .setTitle('**Information**')
+                                .setDescription(`${user} as been remove from the blacklist`)
+                            await interaction.editReply({ embeds: [text] })
+                                .catch(err => log.write(err));
                         } else {
-                            log.write( user.id + 'is not in the blacklist');
+                            log.write(user + ' is not in the blacklist');
                             const text = new EmbedBuilder()
                                 .setColor('#FF0000')
                                 .setTitle('**Warrning**')
-                                .setDescription(`${user}  alreadi not in the blacklist`)
-                            await interaction.editReply({ embeds: [text] });
+                                .setDescription(`${user} is not in the blacklist`)
+                            await interaction.editReply({ embeds: [text] })
+                                .catch(err => log.write(err));
                             return;
                         }
 
-                        settings.blacklist.push(user.id);
-
-                        const text = new EmbedBuilder()
-                            .setColor('#245078')
-                            .setTitle('**Information**')
-                            .setDescription(`This channel are now the channel for the ${option} !`)
-                            .setFooter({ text: '/set `option`' })
-
-                        await interaction.editReply({ embeds: [text] });
                         fs.writeFileSync("./settings.json", JSON.stringify(settings));
                         return;
 
                     } else {
 
+                        var user = await interaction.options.getUser('user', true);
+                        var settings = JSON.parse(fs.readFileSync('./settings.json', 'utf8'));
+
+                        if (settings.blacklist.indexOf(user.id) != -1) {
+                            delete blacklist[settings.blacklist.indexOf(user.id)]
+                        } else {
+                            settings.blacklist.push(user.id);
+                            log.write(user.id + 'has been add to the blacklist');
+                            const text = new EmbedBuilder()
+                                .setColor('#245078')
+                                .setTitle('**Information**')
+                                .setDescription(`${user} has been add to the blacklist`)
+                            await interaction.editReply({ embeds: [text] });
+                        }
+
+                        fs.writeFileSync("./settings.json", JSON.stringify(settings));
+                        return;
                     }
-
-
 
                 } catch (error) {
                     log.write(error);
                     const text = new EmbedBuilder()
                         .setColor('#FF0000')
                         .setTitle('**Error**')
-                        .setDescription(`There was an error executing /set ${option} : \n` + '```' + error + '```')
+                        .setDescription(`There was an error executing /blacklist: \n` + '```' + error + '```')
                     await interaction.editReply({ embeds: [text] });
                     return;
                 }
 
             } else {
                 try {
-                    var user = await interaction.options.getUser('user', true);
-                    let settings = JSON.parse(fs.readFileSync('./settings.json', 'utf8'));
+                    var settings = JSON.parse(fs.readFileSync('./settings.json', 'utf8'));
+                    if (settings.blacklist.length == 0) {
+                        const text = new EmbedBuilder()
+                            .setColor('#245078')
+                            .setTitle('**Information**')
+                            .setDescription(`There is not user in the blacklist`)
+                            .setFooter({ text: '/blacklist `option`' })
 
-                    settings.blacklist.push(user.id);
+                        await interaction.editReply({ embeds: [text] });
+                    } else {
+                        var values = ""
+                        for (let index = 0; index < settings.blacklist.length; index++) {
+                            try {
+                                values += "  * <@" + settings.blacklist[index] + `> \n`;
+                            } catch (e) {
+                                console.log(e)
+                            }
+                        }
+                        const text = new EmbedBuilder()
+                            .setColor('#245078')
+                            .setTitle('**Information**')
+                            .setDescription(`User in the blacklist:\n${values}`)
+                            .setFooter({ text: '/blacklist `option`' })
 
-                    const text = new EmbedBuilder()
-                        .setColor('#245078')
-                        .setTitle('**Information**')
-                        .setDescription(`This channel are now the channel for the ${option} !`)
-                        .setFooter({ text: '/set `option`' })
+                        await interaction.editReply({ embeds: [text] });
+                    }
 
-                    await interaction.editReply({ embeds: [text] });
                     fs.writeFileSync("./settings.json", JSON.stringify(settings));
                     return;
 
@@ -95,7 +132,7 @@ module.exports = {
                     const text = new EmbedBuilder()
                         .setColor('#FF0000')
                         .setTitle('**Error**')
-                        .setDescription(`There was an error executing /set ${option} : \n` + '```' + error + '```')
+                        .setDescription(`There was an error executing /blacklist : \n` + '```' + error + '```')
                     await interaction.editReply({ embeds: [text] });
                     return;
                 }
@@ -106,7 +143,7 @@ module.exports = {
             const text = new EmbedBuilder()
                 .setColor('#FF0000')
                 .setTitle('**Error**')
-                .setDescription(`There was an error executing /set ${option} : \n` + '```' + error + '```')
+                .setDescription(`There was an error executing /blacklist : \n` + '```' + error + '```')
             await interaction.editReply({ embeds: [text] });
             return;
         }
