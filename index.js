3459ebb88092a092e81f1a676cc0a370d3adb297
Kochy_Okami <97362574+KochyOkami@users.noreply.github.com>
2022-11-15 23:02:24 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -67,9 +67,9 @@ bot.on("ready", async () => {
             status: "online",
             activities: [{ name: "la version " + config.bot_version }],
         });
-        rest.put(Routes.applicationCommands(bot.user.id), { body: [] })
+        /* rest.put(Routes.applicationCommands(bot.user.id), { body: [] })
             .then(() => console.log('Successfully deleted all commands.'))
-            .catch(console.error);
+            .catch(console.error); */
 
         (async () => {
             //Load all commands.
