b9557a2c00b36c9894f53fecdadaeb4a0921901f
Kochy_Okami <97362574+KochyOkami@users.noreply.github.com>
2022-11-23 16:55:53 +0100

    Add files via upload

--- commands/yaoi.js
+++ commands/yaoi.js
@@ -46,15 +46,16 @@ module.exports = {
                 }
 
                 var headersOpt = {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+                    'Content-Type': 'application/x-www-form-urlencoded'
                 };
                 requests(
                     {
-                        method: 'get',
+                        method: 'POST',
                         url: 'https://api.qwant.com/V3/search/images',
                         form: option,
-                        Jason: true,
                         headers: headersOpt,
+                        json: true,
                     }, async function (error, response, body) {
                         //Print the Response
                         console.log(body)
