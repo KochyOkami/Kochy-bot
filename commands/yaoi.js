3a79ce26d9dc235cbb1ddd4e0a15cf19a5a6f661
Kochy_Okami <97362574+KochyOkami@users.noreply.github.com>
2022-11-23 20:33:52 +0100

    Add files via upload

--- commands/yaoi.js
+++ commands/yaoi.js
@@ -31,61 +31,75 @@ module.exports = {
             var searchtext = 'yaoi'
             if (await interaction.options.getString('option', false)) {
                 searchtext = "yaoi " + await interaction.options.getString('option', true);
-               
+
             }
             try {
-                //Custom Header pass
-                //console.log('fr')
-
-                var offset =Math.floor(Math.random() * (100))
+                var offset = Math.floor(Math.random() * (5))
                 var headersOpt = {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
 
                 };
-                requests(
-                    {
-                        method: 'get',
-                        url: 'https://api.qwant.com/V3/search/images/?count=50&q='+encodeURI(searchtext)+'&safesearch=0&locale=en_us&offset='+offset.toString(),
-                        headers: headersOpt,
-                    }, async function (error, response, body) {
-                        //Print the Response
-                        //console.log(body)
-                        if (body){
-                        var img = JSON.parse(body)
-                        var url = img.data.result.items[Math.floor(Math.random() * (img.data.result.total))].media
-                        log.write(searchtext+ ' ' + url)
-                        const text = new EmbedBuilder()
-                            .setColor('#6c3483')
-                            .setTitle('**Yaoi**')
-                            .setDescription(`Here is a image for your search: ${searchtext}`)
-                            .setImage(url)
-                            .setFooter({text:interaction.user.tag, iconURL:interaction.user.displayAvatarURL({ extension: 'jpg' })})
-                        await interaction.editReply({
-                            embeds: [text]
-                });
-                        }
-                        else{
-                            
-                            const text = new EmbedBuilder()
+                try {
+                    requests(
+                        {
+                            method: 'get',
+                            url: 'https://api.qwant.com/V3/search/images/?count=80&q='+encodeURI(searchtext)+'&safesearch=0&locale=en_us&offset='+offset.toString(),
+                            headers: headersOpt,
+                        }, async function (error, response, body) {
+                            //Print the Response
+                           
+                            try {
+                                if (body) {
+                                    var img = JSON.parse(body)
+                                    var url = img.data.result.items[Math.floor(Math.random() * (img.data.result.total))].media
+                                    log.write(searchtext+ ' ' + url)
+                                    const text = new EmbedBuilder()
+                                    .setColor('#6c3483')
+                                    .setTitle('**Yaoi**')
+                                    .setDescription(`Here is a image for your search: ${searchtext}`)
+                                    .setImage(url)
+                                    .setFooter({
+                                        text: interaction.user.tag, iconURL: interaction.user.displayAvatarURL({
+                                            extension: 'jpg'
+                                        })})
+                                    await interaction.editReply({
+                                        embeds: [text]
+                                    });
+                                } else {
+                                    log.write(body);
+                                    const text = new EmbedBuilder()
+                                    .setColor('#C0392B')
+                                    .setTitle('**Error**')
+                                    .setDescription(`There was an error executing /yaoi : \n` + '```' + body + '```')
+                                    await interaction.editReply({
+                                        embeds: [text]
+                                    });
+                                    return;
+                                }
+                            }catch (e) {
+                                log.write(error);
+                                const text = new EmbedBuilder()
+                                .setColor('#C0392B')
+                                .setTitle('**Error**')
+                                .setDescription(`There was an error executing /yaoi : \n` + '```' + error + '```')
+                                await interaction.editReply({
+                                    embeds: [text]
+                                });
+                                return;
+                            }
 
-                .setColor('#C0392B')
-
-                .setTitle('**Error**')
-
-                .setDescription(`There was an error executing /yaoi`)
-
-                await interaction.editReply({
-
-                    embeds: [text]
-
-                });
-                        }
-
+                        });
+                }catch (e) {
+                    log.write(error);
+                    const text = new EmbedBuilder()
+                    .setColor('#C0392B')
+                    .setTitle('**Error**')
+                    .setDescription(`There was an error executing /yaoi : \n` + '```' + error + '```')
+                    await interaction.editReply({
+                        embeds: [text]
                     });
-
-                //.setAuthor({ name: `${interaction.user.username}`, iconURL: `${interaction.user.avatarURL({ dynamic: true, size: 512 })}` })
-                // await interaction.editReply({ files: [attachment] });
-                //await interaction.editReply('finished')
+                    return;
+                }
                 return;
 
             } catch (error) {
@@ -93,7 +107,7 @@ module.exports = {
                 const text = new EmbedBuilder()
                 .setColor('#C0392B')
                 .setTitle('**Error**')
-                .setDescription(`There was an error executing /cookie : \n` + '```' + error + '```')
+                .setDescription(`There was an error executing /yaoi : \n` + '```' + error + '```')
                 await interaction.editReply({
                     embeds: [text]
                 });
@@ -107,7 +121,7 @@ module.exports = {
             const text = new EmbedBuilder()
             .setColor('#C0392B')
             .setTitle('**Error**')
-            .setDescription(`There was an error executing /cookie : \n` + '```' + error + '```')
+            .setDescription(`There was an error executing /yaoi : \n` + '```' + error + '```')
             await interaction.editReply({
                 embeds: [text]
             });
