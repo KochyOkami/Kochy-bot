2182e6ad4854f2b2008b85b3b3de72f35b50a959
Kochy_Okami <97362574+KochyOkami@users.noreply.github.com>
2022-11-23 16:04:03 +0100

    Add files via upload

---
+++ commands/yaoi.js
@@ -0,0 +1,79 @@
+const { EmbedBuilder, MessageAttachment } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const log = require('../logs/logBuilder.js');
+const fs = require('fs');
+const { PermissionFlagsBits, AttachmentBuilder } = require('discord.js');
+var requests = require('request');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("yaoi")
+        .setDescription("Give you a yaoi picture ^^")
+        .addStringOption(option =>
+            option.setName('option')
+                .setDescription('key word for the research')
+                .setRequired(false)),
+
+    async execute(interaction) {
+        await interaction.deferReply();
+        try {
+            var settings = JSON.parse(fs.readFileSync('./settings.json', 'utf8'));
+            if (true) {
+                var searchtext = 'yaoi'
+                if (await interaction.options.getString('option', false)) {
+                    searchtext = await interaction.options.getString('option', true);
+                }
+                try {
+                    //Custom Header pass
+                    var option = {
+                            'count': 80,
+                            'q': 'yaoi',
+                            't': 'images',
+                            'safesearch': 0,
+                            'locale': 'en_US',
+                            'offset': 0,
+                            'device': 'desktop'}
+                            
+                    var headersOpt ={
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+                    };
+                    requests(
+                        {
+                            method: 'get',
+                            url: 'https://api.qwant.com/V3/search/images',
+                            query: option,
+                            headers: headersOpt,
+                        }, function (error, response, body) {
+                            //Print the Response
+                            console.log(error, response, body)
+                        });
+                       
+                    //.setAuthor({ name: `${interaction.user.username}`, iconURL: `${interaction.user.avatarURL({ dynamic: true, size: 512 })}` })
+                   // await interaction.editReply({ files: [attachment] });
+                    return;
+
+                } catch (error) {
+                    log.write(error);
+                    const text = new EmbedBuilder()
+                        .setColor('#C0392B')
+                        .setTitle('**Error**')
+                        .setDescription(`There was an error executing /cookie : \n` + '```' + error + '```')
+                    await interaction.editReply({ embeds: [text] });
+                    return;
+                }
+            }
+
+
+        } catch (error) {
+            log.write(error);
+            const text = new EmbedBuilder()
+                .setColor('#C0392B')
+                .setTitle('**Error**')
+                .setDescription(`There was an error executing /cookie : \n` + '```' + error + '```')
+            await interaction.editReply({ embeds: [text] });
+            return;
+        }
+
+    }
+};
