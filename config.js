9692d62d4ad2b66ddb06622059e584cb04b8ad4c
kochyokami@gmail.com <kochyokami@gmail.com>
2022-12-29 00:25:09 +0100

    modified:   config.js     modified:   settings.json

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     commands: [],
-    bot_version: "2.7.0",
+    bot_version: "2.8.0",
     avatar : 'https://www.yaoicute.com/images/avatar.png',
     unknown_book : 'https://www.yaoicute.com/images/avatar.png',
-    debug : true
+    debug : false
     
 }
 
